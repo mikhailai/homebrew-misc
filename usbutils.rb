0e6275c9de9f0611b4816f9e076e76416b877a6f
Mikhail Iakhiaev <mikhailai@gmail.com>
2018-08-13 08:55:20 +0300

    Comments and minor update: no functional changes.
    Submitting to reflect the latest status of usbutils.rb
    while trying to push it to homebrew-core.

--- usbutils.rb
+++ usbutils.rb
@@ -1,15 +1,9 @@
 class Usbutils < Formula
-  # Some of the information is based on Macports 'usbutils':
-  # https://github.com/macports/macports-ports/blob/master/sysutils/usbutils/Portfile
   desc "Provides 'lsusb' tool for getting detailed info about USB devices"
-  # This is actually a homepage for a bunch of Linux USB things, 'usbutils' is
-  # among them:
+  # Homepage for multiple Linux USB tools, 'usbutils' is one of them.
   homepage "https://linux-usb.sourceforge.io"
-  # The 'usbutils' package contains several utilities, but all the ones besides
-  # 'lsusb' are linux-specific (use sysfs).
-  # Note, the latest version we can use is 007 (from 2013), because the later
-  # versions depend on 'libudev'. The 'libudev' is not in Homebrew and is a
-  # part of Systemd - may be very hard to port.
+  # The latest version we can use is 007 (from 2013), because the later
+  # versions depend on 'libudev': part of Systemd and not in Homebrew.
   url "https://mirrors.edge.kernel.org/pub/linux/utils/usb/usbutils/usbutils-007.tar.xz"
   sha256 "7593a01724bbc0fd9fe48e62bc721ceb61c76654f1d7b231b3c65f6dfbbaefa4"
 
@@ -24,18 +18,19 @@ class Usbutils < Formula
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
-    # Clear/override several variables, to avoid building/installing
-    # utilities that don't work on Mac.
-    system "make", "install", "SUBDIRS=", "AM_LDFLAGS=", "bin_SCRIPTS=",
+    # Clear AM_LDFLAGS to remove unsupported "-Wl,--as-needed"
+    # Also, clear/override several other variables, to avoid
+    # building/installing linux-specific utilities that rely on sysfs.
+    system "make", "install", "AM_LDFLAGS=", "SUBDIRS=", "bin_SCRIPTS=",
                               "man_MANS=lsusb.8"
   end
 
   test do
-    # Run --version only. The normal 'lsusb' run may exit with 1 if executed
-    # on a Virtual Machine.
+    # Run --version only. The normal 'lsusb' run may exit
+    # with 1 if executed on a Virtual Machine.
     system "#{bin}/lsusb", "--version"
-    # Make sure it does not print anything to stderr - it would print
-    # a message if it could not find the USB ids:
+    # Make sure nothing shows up on stderr - 'lsusb' would print
+    # a message if it could not find the USB ids file:
     _, stderr, status = Open3.capture3("#{bin}/lsusb")
     assert_true status.exited?
     assert_equal "", stderr
