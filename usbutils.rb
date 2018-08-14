beb4616f40f2e56ec3efb4751c34b62f8eba6cf7
Mikhail Iakhiaev <mikhailai@gmail.com>
2018-08-14 11:25:34 +0300

    Adding the binary bottles - just like the real HB

--- usbutils.rb
+++ usbutils.rb
@@ -7,6 +7,13 @@ class Usbutils < Formula
   url "https://mirrors.edge.kernel.org/pub/linux/utils/usb/usbutils/usbutils-007.tar.xz"
   sha256 "7593a01724bbc0fd9fe48e62bc721ceb61c76654f1d7b231b3c65f6dfbbaefa4"
 
+  bottle do
+    root_url "https://dl.bintray.com/mikhailai/bottles-misc"
+    sha256 "b8de8fc8be85c77d47b3251dc32153a7bcc43fd7b143069c0f9baa6ae0e90208" => :el_capitan
+    sha256 "5e9e8018181e361e61921c4616d3eb0b98691faafce7023e8764f53854b0b53a" => :sierra
+    sha256 "d16701763632378ab44c00efd9067edec9497df38f29365147ce51fe1552b071" => :high_sierra
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
